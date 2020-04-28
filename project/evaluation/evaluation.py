56775ee3fd714664a3a57dc9726662aa5ad74401
emaeuer <43860315+emaeuer@users.noreply.github.com>
2020-04-28 13:35:45 +0200

    Adjusted data evaluation

---
+++ project/evaluation/evaluation.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+
+def read_mapping_file(filename):
+    """Create a dictionary which contains the mappings of the file"""
+    mappings = {}
+    with open(filename) as mapping_file:
+        for mapping in mapping_file:
+            value, keys = mapping.strip().split(":")
+            if value is "":
+                continue
+            for key in keys.split(","):
+                mappings[key] = value
+    return mappings
+
+def evaluate_data(data_frame, category_name):
+    """Create a plot for the column of a data frame"""
+    series = data_frame[category_name]
+    counts_of_groups = series.value_counts()
+    counts_of_groups.plot.bar(y="count", rot=0)
+    counts_of_groups.plot.bar(y="count", rot=0)
+    plt.savefig(f"project/evaluation/results/{category_name}.png")
+    plt.close()
+
+# read csv
+with open("styles.csv") as file:
+    df = pd.read_csv(file, usecols=[0, 1, 4, 5])
+
+    df.gender = df.gender.map(read_mapping_file("project/evaluation/mappings/gender_mapping.txt"))
+    df.baseColour = df.baseColour.map(read_mapping_file("project/evaluation/mappings/color_mapping.txt"))
+    # TODO create mappings for articleType
+    # df.articleType = df.articleType.map(read_mapping_file("project/evaluation/mappings/article_mapping.txt"))
+
+    # remove lines which contain empty values
+    df = df.dropna()
+
+    df.to_csv("styles_edited.csv", index=False)
+
+    evaluate_data(df, "gender")
+    evaluate_data(df, "baseColour")
+    # TODO evaluate_data(df, "articleType")
