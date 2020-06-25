1a4fa46f275beb51cd8024c83e90e7b17cef39f9
je704xylo <je704xylo@clara28.sc.uni-leipzig.de>
2020-06-25 09:39:26 +0200

    Multi GPU support

--- project/model.py
+++ project/model.py
@@ -10,32 +10,43 @@ class Model:
         self.create_model()
 
     def create_model(self):
-        base_model = tf.keras.applications.NASNetLarge(include_top=False,
-                                                       weights='imagenet')
-        base_model.trainable = False
-        global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
-        prediction_layer_one = tf.keras.layers.Dense(128)
-        prediction_layer_two = tf.keras.layers.Dense(39, activation='sigmoid')
+        mirrored_strategy = tf.distribute.MirroredStrategy()
+        
+        with mirrored_strategy.scope(): 
+            base_model = tf.keras.applications.NASNetLarge(include_top=False,
+                                                       weights='imagenet',classes=39)
+            base_model.trainable = True
+            set_trainable = False
+            for layer in base_model.layers:
+                if layer.name == 'activation_166':
+                    set_trainable = True
+                if set_trainable:
+                   layer.trainable = True
+                else:
+                   layer.trainable = False
+            global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+            prediction_layer_one = tf.keras.layers.Dense(128)
+            prediction_layer_two = tf.keras.layers.Dense(39, activation='sigmoid')
 
-        self.model = tf.keras.Sequential([
-            base_model, global_average_layer, prediction_layer_one,
-            prediction_layer_two
-        ])
+            self.model = tf.keras.Sequential([
+                base_model, global_average_layer, prediction_layer_one,
+                prediction_layer_two
+            ])
 
         self.model.compile(optimizer=tf.keras.optimizers.Adam(),
                            loss=tf.keras.losses.BinaryCrossentropy(),
                            metrics=['accuracy'])
 
     def fit(self):
-        log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+        #log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        #tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
 
         self.model.fit(self.ds.train,
                        steps_per_epoch=44199 // Config.BATCH_SIZE,
                        epochs=1,
                        validation_data=self.ds.val,
                        validation_steps=4444 // Config.BATCH_SIZE,
-                       callbacks=[tensorboard_callback])
+                       callbacks=[])
 
     def eval(self):
         return self.model.evaluate(self.ds.test,
